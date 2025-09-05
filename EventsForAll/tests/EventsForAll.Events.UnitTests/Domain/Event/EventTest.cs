6c5d72b43c127ac58634400877a77375d2cadefe
Pedro Sandrini <40308127+pedrosandrini@users.noreply.github.com>
2025-09-05 19:39:53 +0000

    Add unit test for event creation with valid data

---
+++ EventsForAll/tests/EventsForAll.Events.UnitTests/Domain/Event/EventTest.cs
@@ -0,0 +1,29 @@
+using EventDomain = EventsForAll.Events.Domain.Entities.Event;
+
+namespace EventsForAll.Events.UnitTests.Domain.Event;
+
+public class EventTest
+{
+    [Fact(DisplayName = "")]
+    [Trait("Event", "Unit")]
+    public void CreateEvent_ShouldCreateEvent_WhenDataIsValid()
+    {
+        var validData = new EventDomain.CreateEventCommand
+        {
+            Title = "Sample Event",
+            Description = "This is a sample event description.",
+            StartDate = DateTime.UtcNow.AddDays(1),
+            EndDate = DateTime.UtcNow.AddDays(2),
+            Location = "Sample Location"
+        };
+
+        var newEvent = EventDomain.Event.Create(validData);
+
+        Assert.NotNull(newEvent);
+        Assert.Equal(validData.Title, newEvent.Title);
+        Assert.Equal(validData.Description, newEvent.Description);
+        Assert.Equal(validData.StartDate, newEvent.StartDate);
+        Assert.Equal(validData.EndDate, newEvent.EndDate);
+        Assert.Equal(validData.Location, newEvent.Location);
+    }
+}
